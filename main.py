41e68595f7ddc8faf89251c9f702e3f38393d015
Andre Gonzalez <andagonmaster@outlook.com>
2024-11-27 09:26:11 -0600

    new EPs, probando

--- main.py
+++ main.py
@@ -106,6 +106,87 @@ def get_user_info(user_id):
         if connection is not None:
             connection.close()
 
+# POST URL
+@app.route('/urls', methods=['POST'])
+def create_url():
+    data = request.get_json()  # Obtén los datos JSON de la solicitud
+    
+    # Extrae los valores del JSON
+    name_url = data.get('name_url')
+    destination = data.get('destination')
+    user_id = data.get('user_id')
+
+    # Verifica que los datos necesarios estén presentes
+    if not name_url or not destination or user_id is None:
+        return jsonify({"message": "Faltan datos obligatorios"}), 400
+
+    connection = None
+    cursor = None
+
+    try:
+        connection = get_db_connection()
+        cursor = connection.cursor()
+
+        # Ejecuta el INSERT
+        cursor.execute('''
+            INSERT INTO goshort.pro.url (name_url, destination, user_id)
+            VALUES (%s, %s, %s)
+            RETURNING url_id;
+        ''', (name_url, destination, user_id))
+        
+        # Obtén el ID de la nueva URL insertada
+        new_url_id = cursor.fetchone()[0]
+
+        # Confirma la transacción
+        connection.commit()
+
+        return jsonify({"message": "URL creada exitosamente", "url_id": new_url_id}), 201
+
+    except Exception as e:
+        print(f"Error al crear la URL: {e}")
+        if connection:
+            connection.rollback()  # Revierte en caso de error
+        return jsonify({"message": "Error del servidor", "error": str(e)}), 500
+
+    finally:
+        if cursor is not None:
+            cursor.close()
+        if connection is not None:
+            connection.close()
+
+# GET URL
+@app.route('/user/<string:user_id>/urls', methods=['GET'])
+def get_user_urls(user_id):
+    connection = get_db_connection()
+    cursor = connection.cursor()
+
+    try:
+        # Consulta para obtener las URLs asociadas al usuario
+        cursor.execute('''
+            SELECT url.name_url
+            FROM goshort.pro.url url
+            WHERE url.user_id = %s;
+        ''', (user_id,))
+        
+        urls = cursor.fetchall()
+
+        # Verificar si el usuario tiene URLs
+        if not urls:
+            return jsonify({"message": "No URLs found for this user"}), 404
+
+        # Convertir los resultados en una lista de diccionarios
+        urls_list = [{"name_url": url[0]} for url in urls]
+
+        return jsonify(urls_list), 200
+
+    except Exception as e:
+        print(f"Error al obtener las URLs del usuario: {e}")
+        return jsonify({"message": "Error del servidor", "error": str(e)}), 500
+
+    finally:
+        cursor.close()
+        connection.close()            
+
 # Ejecuta la aplicación
 if __name__ == '__main__':
     app.run(debug=True)
